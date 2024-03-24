1adadf7606d1dfff68fe2bda70ad1cc5df7b0a86
zelda <lichtseeker@gmail.com>
2024-03-24 09:55:43 -0700

    Refactor Profile Screen: put signOut function here

--- components/Profile.js
+++ components/Profile.js
@@ -1,12 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { auth } from "../firebase-files/firebaseSetup";
+import PressableButton from "./PressableButton";
+import { signOut } from "firebase/auth";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Profile() {
   return (
     <View>
       <Text>{auth.currentUser.uid}</Text>
       <Text>{auth.currentUser.email}</Text>
+      <PressableButton
+        onPress={() => {
+          signOut(auth)
+            .catch((error) => console.error("Error signing out:", error));
+        }}
+        customStyle={styles.buttonStyle}
+      >
+        <Ionicons name="log-out-outline" size={24} color="white" /> 
+      </PressableButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: 'tomato',
+    borderRadius: 5,
+    width: 50,
+  },
+});
