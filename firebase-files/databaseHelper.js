7ecd640a9028a65b401e0dfb64b97e61f9ea79a2
zelda <lichtseeker@gmail.com>
2024-03-24 17:47:14 -0700

    Added two function: fetchUserData from database, updateUserProfile to database

---
+++ firebase-files/databaseHelper.js
@@ -0,0 +1,34 @@
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { database } from "./firebaseSetup";
+
+export const fetchUserData = async (userId) => {
+    try {
+      const userDocRef = doc(database, "users", userId);
+      const docSnap = await getDoc(userDocRef);
+      if (docSnap.exists()) {
+        return docSnap.data();
+      } else {
+        return null; // Handle case where user data does not exist
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+      throw error;
+    }
+  };
+
+export const updateUserProfile = async (userId, newUsername, email) => {
+    try {
+        const userRef = doc(database, "users", userId);
+        const userData = {
+            username: newUsername,
+            email: email,
+        };
+
+        await setDoc(userRef, userData, { merge: true });
+        console.log("User profile updated in Firestore.");
+        return "Profile updated successfully"; // This value is resolved with the promise
+    } catch (error) {
+        console.error("Error updating user profile:", error);
+        throw error; // This will reject the promise with the error
+    }
+};
