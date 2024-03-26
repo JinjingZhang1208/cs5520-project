892d23c3f1856212e6015019d369a1ef67d95ea0
yolandahe <he.yang7@northeastern.edu>
2024-03-25 23:36:05 -0700

    added card component

---
+++ styles/CommonStyles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "rgb(60,61,132)",
+        width:'100%',
+        height: 40,
+        borderRadius:5,
+        justifyContent: 'center',
+        shadowRadius:3,
+        shadowOpacity: 0.2,
+        marginTop:'5%',
+    },
+})
