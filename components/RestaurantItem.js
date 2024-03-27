bcbea023ae60aec472b25d136e96fcacc62a72d2
yolandahe <he.yang7@northeastern.edu>
2024-03-26 17:18:59 -0700

    added restaurant list and send it to discover page

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 import Card from './Card';
--- screens/Discover.js
+++ screens/Discover.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import RestaurantList from '../components/RestaurantList';
 
 export default function Discover({ navigation }) {
   return (
     <View>
-      <Text>Discover</Text>
-    
+      <RestaurantList />
     </View>
   );
 }
