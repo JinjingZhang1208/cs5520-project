1920a5a1c3279d4374d9012b4bec543a9ac9ce9d
zelda <lichtseeker@gmail.com>
2024-03-27 22:51:26 -0700

    Fetch restaurant from API in Discover screen

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, Alert } from 'react-native';
 import RestaurantList from '../components/RestaurantList';
 import CommonStyles from '../styles/CommonStyles';
+import { fetchAndPrepareRestaurants } from '../services/YelpService';
 
 export default function Discover({ navigation }) {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const getRestaurants = async () => {
+      try {
+        const data = await fetchAndPrepareRestaurants();
+        setRestaurants(data);
+      } catch (error) {
+        Alert.alert('Error', 'Unable to fetch restaurants.');
+        console.error(error);
+      }
+    };
+
+    getRestaurants();
+  }, []);
+  
   return (
     <View style={CommonStyles.container}>
       <View style={{marginTop:10}}>
-        <RestaurantList collectionName='restaurants'/>
+        <RestaurantList fetchedRestaurants={restaurants} collectionName='restaurants'/>
       </View>
     </View>
   );
