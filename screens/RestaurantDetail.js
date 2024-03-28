b4651a5ffb7aacb56d019ab1c41190b53af130c7
zelda <lichtseeker@gmail.com>
2024-03-28 00:22:06 -0700

    Update res info for RestaurantDetail screen

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,19 +3,20 @@ import React, { useEffect, useState } from 'react'
 import CommonStyles from '../styles/CommonStyles'
 import PressableButton from '../components/PressableButton'
 import { MaterialIcons } from '@expo/vector-icons';
-import { auth, database} from '../firebase-files/firebaseSetup';
-import {writeToDB, deleteFromDB} from '../firebase-files/databaseHelper';
+import { auth, database } from '../firebase-files/firebaseSetup';
+import { writeToDB, deleteFromDB } from '../firebase-files/databaseHelper';
 import { doc, getDoc } from "firebase/firestore";
+import Card from '../components/Card';
 
-export default function RestaurantDetail({navigation, route}) {
+export default function RestaurantDetail({ navigation, route }) {
     const [bookmark, setBookmark] = useState(false);
 
     //check if the restaurant is in the wishlist
     useEffect(() => {
         checkIfInWishList();
-    },[bookmark]);
+    }, [bookmark]);
 
-    async function checkIfInWishList () {
+    async function checkIfInWishList() {
         const currentUser = auth.currentUser;
         if (currentUser) {
             const userId = currentUser.uid;
@@ -34,23 +35,24 @@ export default function RestaurantDetail({navigation, route}) {
     }
 
     //add or remove the restaurant from the wishlist
-    async function wishlistHandler () {
+    async function wishlistHandler() {
         const currentUser = auth.currentUser;
         if (currentUser) {
             const userId = currentUser.uid;
             const restaurantId = route.params.item.id;
 
             try {
-                if (bookmark)  {
+                if (bookmark) {
                     await deleteFromDB('users', userId, 'wishlists', restaurantId);
                     setBookmark(false);
                     Alert.alert('Removed from Wishlist');
                 } else {
                     let res = {
-                        restaurantId: restaurantId, 
+                        restaurantId: restaurantId,
                         name: route.params.item.name,
                         rating: route.params.item.rating,
-                        numOfComments: route.params.item.numOfComments,};
+                        numOfComments: route.params.item.numOfComments,
+                    };
                     await writeToDB(res, 'users', userId, 'wishlists');
                     setBookmark(true);
                     Alert.alert('Added to Wishlist');
@@ -62,22 +64,28 @@ export default function RestaurantDetail({navigation, route}) {
     }
 
     //set header right to a button that adds the restaurant to the wishlist
-    useEffect (() => { 
+    useEffect(() => {
         navigation.setOptions({
             headerRight: () => (
                 <PressableButton onPress={wishlistHandler}>
-                    {bookmark? <MaterialIcons name="bookmark-added" size={24} color="black" /> :
-                    <MaterialIcons name="bookmark-add" size={24} color="black" />}
+                    {bookmark ? <MaterialIcons name="bookmark-added" size={24} color="black" /> :
+                        <MaterialIcons name="bookmark-add" size={24} color="black" />}
                 </PressableButton>
             )
-        });   
-    },[bookmark]);
+        });
+    }, [bookmark]);
 
     return (
-        <View style={[{marginTop: 10}, CommonStyles.container]}>
-        <Image 
-            source={require('../assets/restaurant.jpeg')} 
-            style={{width: 325, height: 100}} />
+        <View style={[{ marginTop: 10 }, CommonStyles.container]}>
+            <Card>
+                <Image
+                    source={{ uri: route.params.item.image_url }}
+                    style={{ width: 325, height: 100 }} />
+                <View style={[CommonStyles.directionRow, { justifyContent: 'start' }]}>
+                    <Text>Ratings: {route.params.item.rating}   </Text>
+                    <Text>Reviews: {route.params.item.review_count}</Text>
+                </View>
+            </Card>
         </View>
     )
 }
