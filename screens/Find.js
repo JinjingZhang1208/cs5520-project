fd24f08db8d3f4233464fb2ef9e3dd9f34445154
zelda <lichtseeker@gmail.com>
2024-04-07 15:56:10 -0700

    Add input bar

--- screens/Find.js
+++ screens/Find.js
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, {useState} from 'react'
+import PressableButton from '../components/PressableButton'
 
 const Find = () => {
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [searchDistance, setSearchDistance] = useState(5);
+  const [searchRating, setSearchRating] = useState(4);
+
   return (
-    <View>
-      <Text>Search restaurants here!</Text>
+    <View style={styles.container}>
+      <TextInput style={styles.TextInput} 
+        placeholder='Search for restaurants' 
+        onChangeText={setSearchKeyword}
+        value={searchKeyword}
+      />
+      
     </View>
   )
 }
 
 export default Find
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  TextInput: {
+    marginTop: 50,
+    marginLeft: 30,
+    marginRight: 30,
+    marginBottom: 30,
+    height: 40,
+    padding: 10,
+    borderRadius: 3,
+    borderWidth: 2,
+  }
+})
