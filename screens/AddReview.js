a765792d59cff1fecf62997813747d460e9f273f
yolandahe <he.yang7@northeastern.edu>
2024-04-10 05:29:15 -0700

    add comments for add review page

--- screens/AddReview.js
+++ screens/AddReview.js
@@ -31,7 +31,7 @@ export default function Review({navigation, route}) {
                 bussiness_id: route.params.item.bussiness_id,
                 restaurantName: route.params.item.name, 
                 owner: userId};
-            // writeToDB(newReview, 'users', userId, 'reviews'); // write to user's reviews
+
             writeToDB(newReview, 'allReviews'); // write to all reviews
             navigation.goBack();
         }
@@ -49,41 +49,40 @@ export default function Review({navigation, route}) {
                 owner: userId
             };
 
-            console.log('updatedReview:', updatedReview);
-            // updateDB(updatedReview, 'users', userId, 'reviews', route.params.review.id);// update user's review
             updateDB(updatedReview, 'allReviews', route.params.review.id); // update all reviews
             navigation.goBack();
         }
     }
 
-    function openImageModal() {
-        setImageModalVisible(true);
-    }
-
-    function dismissModal() {
-        setImageModalVisible(false);
-    }
-
     return (
         <View style={CommonStyles.container}>
-            <View style={{alignSelf:'flex-start'}}>
+            {/* Add a button to open the image modal */}
+            <View style={{alignSelf:'flex-start', marginLeft: 60}}>
                 <PressableButton
-                    onPress={openImageModal}>
+                    onPress={() => setImageModalVisible(true)}>
                     <MaterialIcons name="add-a-photo" size={40} color="black" />
                 </PressableButton>
             </View>
+            
+            {/* Add the ImageInput modal */}
             <ImageInput
                 imageModalVisible={imageModalVisible}
-                dismissModal={dismissModal}
+                dismissModal={() => setImageModalVisible(false)}
             />
-            <View style={{marginTop:10}}>   
+
+            <View style={{marginTop:10}}> 
+                {/* Add a text input for the review content */}
                 <TextInput 
                     placeholder='Enter your review'
                     style={CommonStyles.reviewInput}
                     value={reviewContent}
                     onChangeText={setReviewContent}/>
+
+                {/* If in edit mode, display the restaurant name */}
                 {mode == 'edit'? <Text>{route.params.review.restaurantName}</Text> :
                     <Text>{route.params.item.name}</Text>}
+
+                {/* Add a button to submit the review */}
                 <PressableButton 
                     customStyle={styles.pressableButtonStyle}
                     onPress={mode == 'edit'? editHandler:submitHandler}>
