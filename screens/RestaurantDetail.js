ae78642eb3a6f34e1accfbe11bdd0b5a664bb4c9
zelda <lichtseeker@gmail.com>
2024-04-12 21:55:10 -0700

    Minor change: Update indent from 4 to 2

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -13,192 +13,199 @@ import { Rating } from 'react-native-ratings';
 
 
 export default function RestaurantDetail({ navigation, route }) {
-    const [bookmark, setBookmark] = useState(false);
-    const [myReviews, setMyReviews] = useState([]);
-    const [otherReviews, setOtherReviews] = useState([]);
+	const [bookmark, setBookmark] = useState(false);
+	const [myReviews, setMyReviews] = useState([]);
+	const [otherReviews, setOtherReviews] = useState([]);
 
-    //get restaurant details from the route params
-    const restaurantId = route.params.item.bussiness_id;
-    const name = route.params.item.name;
-    const rating = route.params.item.rating;
-    const review_count = route.params.item.review_count;
-    const image_url = route.params.item.image_url;
-    const latitude = route.params.item.latitude;
-    const longitude = route.params.item.longitude;
-    const address = route.params.item.address;
-    const phone = route.params.item.phone;
-    const price = route.params.item.price;
-    const menu = route.params.item.menu;
+	//get restaurant details from the route params
+	const restaurantId = route.params.item.bussiness_id;
+	const name = route.params.item.name;
+	const rating = route.params.item.rating;
+	const review_count = route.params.item.review_count;
+	const image_url = route.params.item.image_url;
+	const latitude = route.params.item.latitude;
+	const longitude = route.params.item.longitude;
+	const address = route.params.item.address;
+	const phone = route.params.item.phone;
+	const price = route.params.item.price;
+	const menu = route.params.item.menu;
 
-    //check if the restaurant is in the wishlist
-    useEffect(() => {
-        checkIfInWishList();
-    }, [bookmark]);
+	//check if the restaurant is in the wishlist
+	useEffect(() => {
+		checkIfInWishList();
+	}, [bookmark]);
 
-    async function checkIfInWishList() {
-        const currentUser = auth.currentUser;
-        if (currentUser) {
-            const userId = currentUser.uid;
-            const docRef = doc(database, 'users', userId, 'wishlists', restaurantId);
-            const docSnap = await getDoc(docRef);
+	async function checkIfInWishList() {
+		const currentUser = auth.currentUser;
+		if (currentUser) {
+			const userId = currentUser.uid;
+			const docRef = doc(database, 'users', userId, 'wishlists', restaurantId);
+			const docSnap = await getDoc(docRef);
 
-            try {
-                if (docSnap.exists()) {
-                    setBookmark(true);
-                }
-            } catch (error) {
-                console.error('Error checking wishlist:', error);
-            }
-        }
-    }
+			try {
+				if (docSnap.exists()) {
+					setBookmark(true);
+				}
+			} catch (error) {
+				console.error('Error checking wishlist:', error);
+			}
+		}
+	}
 
-    //add or remove the restaurant from the wishlist
-    async function wishlistHandler() {
-        const currentUser = auth.currentUser;
-        if (currentUser) {
-            const userId = currentUser.uid;
+	//add or remove the restaurant from the wishlist
+	async function wishlistHandler() {
+		const currentUser = auth.currentUser;
+		if (currentUser) {
+			const userId = currentUser.uid;
 
-            try {
-                if (bookmark) {
-                    await deleteFromDB('users', userId, 'wishlists', restaurantId);
-                    setBookmark(false);
-                    Alert.alert('Removed from Wishlist');
-                } else {
-                    let res = {
-                        bussiness_id: restaurantId,
-                        name: name,
-                        rating: rating,
-                        review_count: review_count,
-                        image_url: image_url,
-                        owner: userId,
-                        latitude: latitude,
-                        longitude: longitude,
-                        address: address,
-                        phone: phone,
-                        price: price,
-                        menu: menu
-                    };
-                    await writeToDB(res, 'users', userId, 'wishlists');
-                    setBookmark(true);
-                    Alert.alert('Added to Wishlist');
-                }
-            } catch (error) {
-                console.error('Error adding to wishlist:', error);
-            }
-        }
-    }
+			try {
+				if (bookmark) {
+					await deleteFromDB('users', userId, 'wishlists', restaurantId);
+					setBookmark(false);
+					Alert.alert('Removed from Wishlist');
+				} else {
+					let res = {
+						bussiness_id: restaurantId,
+						name: name,
+						rating: rating,
+						review_count: review_count,
+						image_url: image_url,
+						owner: userId,
+						latitude: latitude,
+						longitude: longitude,
+						address: address,
+						phone: phone,
+						price: price,
+						menu: menu
+					};
+					await writeToDB(res, 'users', userId, 'wishlists');
+					setBookmark(true);
+					Alert.alert('Added to Wishlist');
+				}
+			} catch (error) {
+				console.error('Error adding to wishlist:', error);
+			}
+		}
+	}
 
-    //set header right to a button that adds the restaurant to the wishlist
-    useEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <PressableButton onPress={wishlistHandler}>
-                    {bookmark ? <MaterialIcons name="bookmark-added" size={24} color="black" /> :
-                        <MaterialIcons name="bookmark-add" size={24} color="black" />}
-                </PressableButton>
-            )
-        });
-    }, [bookmark]);
+	//set header right to a button that adds the restaurant to the wishlist
+	useEffect(() => {
+		navigation.setOptions({
+			headerRight: () => (
+				<PressableButton onPress={wishlistHandler}>
+					{bookmark ? <MaterialIcons name="bookmark-added" size={24} color="black" /> :
+						<MaterialIcons name="bookmark-add" size={24} color="black" />}
+				</PressableButton>
+			)
+		});
+	}, [bookmark]);
 
-    //fetch reviews for the restaurant use readAllReviewsFromDB
-    useEffect(() => {
-        async function fetchMyReviewsData() {
-            const reviews = await readMyReviewsFromDB(restaurantId);
-            setMyReviews(reviews);
-        }
-        fetchMyReviewsData();
-    }, [myReviews]);
+	//fetch reviews for the restaurant use readAllReviewsFromDB
+	useEffect(() => {
+		async function fetchMyReviewsData() {
+			const reviews = await readMyReviewsFromDB(restaurantId);
+			setMyReviews(reviews);
+		}
+		fetchMyReviewsData();
+	}, [myReviews]);
 
-    //fetch reviews for the restaurant use rea
-    useEffect(() => {
-        async function fetchOtherReviewsData() {
-            const reviews = await readOtherReviewsFromDB(restaurantId);
-            setOtherReviews(reviews);
-            //console.log('otherReviews:', reviews);
-        }
-        fetchOtherReviewsData();
-    }, [otherReviews]);
+	//fetch reviews for the restaurant use rea
+	useEffect(() => {
+		async function fetchOtherReviewsData() {
+			const reviews = await readOtherReviewsFromDB(restaurantId);
+			setOtherReviews(reviews);
+			//console.log('otherReviews:', reviews);
+		}
+		fetchOtherReviewsData();
+	}, [otherReviews]);
 
-    const handleMenuPress = () => {
-        Linking.openURL(menu);
-    };
+	const handleMenuPress = () => {
+		Linking.openURL(menu);
+	};
 
-    return (
-        <View style={[{ marginTop: 10 }, CommonStyles.restaurantContainer]}>
+	return (
+		<View style={[{ marginTop: 10 }, CommonStyles.restaurantContainer]}>
 
-            <Card>
-                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                    <Image
-                        source={{ uri: image_url }}
-                        style={{ width: '90%', height: 150, borderRadius: 5 }} />
-                </View>
-                <View style={{ width: '90%', justifyContent: 'flex-start', alignItems: 'flex-start', marginTop: 7, marginLeft: 16 }}>
-                    <View style={[CommonStyles.directionRow, { justifyContent: 'center' }]}>
-                        <Rating
-                            type="star"
-                            ratingCount={5}
-                            imageSize={15}
-                            startingValue={rating}
-                            fractions={1}
-                            readonly={true}
-                        />
-                        <Text style={{ marginLeft: 5, fontSize: 15  }}>{rating}</Text>
-                        <View style={{ marginLeft: 40 }}>
-                            <MaterialCommunityIcons name="comment" size={14} color="grey" />
-                        </View>
-                        <Text style={{ marginLeft: 3, fontSize: 15 }}> {review_count}</Text>
-                    </View>
-                    <View style={CommonStyles.directionRow}>
-                        <MaterialIcons name="location-on" size={15} color="crimson" />
-                        <Text style={{ marginLeft: 5, fontSize: 15  }}>{address} </Text>
-                    </View>
-                    <View style={CommonStyles.directionRow}>
-                        <Zocial name="call" size={15} color="skyblue" />
-                        <Text style={{ marginLeft: 5, fontSize: 15  }}>{phone} </Text>
-                    </View>
-                    { price != 'N/A' && <View style={CommonStyles.directionRow}>
-                        <FontAwesome name="dollar" size={14} color="goldenrod" />
-                        <Text style={{ marginLeft: 10, fontSize: 15  }}>{price} </Text>
-                    </View> }
-                    { menu != null &&<Pressable onPress={handleMenuPress}>
-                        <View style={CommonStyles.directionRow}>
-                            <MaterialIcons name="menu-book" size={15} color="grey" />
-                            <Text style={{ fontSize: 15, color: 'dodgerblue', textDecorationLine: 'underline', marginLeft: 5 }}>{menu}</Text>
-                        </View>
-                    </Pressable> }
-                </View>
-                <PressableButton
-                        customStyle={styles.pressableButtonStyle}
-                        onPress={() => { navigation.navigate('Add My Review', { item: route.params.item }) }}>
-                        <Text>Write my Review</Text>
-                    </PressableButton>
-            </Card>
+			<Card>
+				<View style={{ justifyContent: 'center', alignItems: 'center' }}>
+					<Image
+						source={{ uri: image_url }}
+						style={{ width: '90%', height: 150, borderRadius: 5 }} />
+				</View>
 
-            <Card>
-                <View>
-                    <View style={{ marginBotton: 5 }}>
-                        <Text style={{ fontSize: 15, fontWeight: 'bold' }}>My Reviews</Text>
-                        <ReviewList allReviews={myReviews} />
-                    </View>
-                    <View style={{ marginBotton: 5 }}>
-                        {otherReviews != [] && <Text style={{ fontSize: 15, fontWeight: 'bold' }}>Other Reviews</Text>}
-                        <ReviewList allReviews={otherReviews} />
-                    </View>
-                </View>
-            </Card>
+				<View style={{ width: '90%', justifyContent: 'flex-start', alignItems: 'flex-start', marginTop: 7, marginLeft: 16 }}>
+					<View style={[CommonStyles.directionRow, { justifyContent: 'center' }]}>
+						<Rating
+							type="star"
+							ratingCount={5}
+							imageSize={15}
+							startingValue={rating}
+							fractions={1}
+							readonly={true}
+						/>
+						<Text style={{ marginLeft: 5, fontSize: 15 }}>{rating}</Text>
 
-        </View>
-    )
+						<View style={{ marginLeft: 40 }}>
+							<MaterialCommunityIcons name="comment" size={14} color="lightgrey" />
+						</View>
+						<Text style={{ marginLeft: 3, fontSize: 15 }}> {review_count}</Text>
+					</View>
+
+					<View style={CommonStyles.directionRow}>
+						<MaterialIcons name="location-on" size={15} color="crimson" />
+						<Text style={{ marginLeft: 5, fontSize: 15 }}>{address} </Text>
+					</View>
+
+					<View style={CommonStyles.directionRow}>
+						<Zocial name="call" size={15} color="skyblue" />
+						<Text style={{ marginLeft: 5, fontSize: 15 }}>{phone} </Text>
+					</View>
+
+					{price != 'N/A' && <View style={CommonStyles.directionRow}>
+						<FontAwesome name="dollar" size={14} color="goldenrod" />
+						<Text style={{ marginLeft: 10, fontSize: 15 }}>{price} </Text>
+					</View>}
+
+					{menu != null && <Pressable onPress={handleMenuPress}>
+						<View style={CommonStyles.directionRow}>
+							<MaterialIcons name="menu-book" size={15} color="grey" />
+							<Text style={{ fontSize: 15, color: 'dodgerblue', textDecorationLine: 'underline', marginLeft: 5 }}>{menu}</Text>
+						</View>
+					</Pressable>}
+				</View>
+
+				<PressableButton
+					customStyle={styles.pressableButtonStyle}
+					onPress={() => { navigation.navigate('Add My Review', { item: route.params.item }) }}>
+					<Text>Write my Review</Text>
+				</PressableButton>
+			</Card>
+
+			<Card>
+				<View>
+					<View style={{ marginBotton: 5 }}>
+						<Text style={{ fontSize: 15, fontWeight: 'bold' }}>My Reviews</Text>
+						<ReviewList allReviews={myReviews} />
+					</View>
+					<View style={{ marginBotton: 5 }}>
+						{otherReviews != [] && <Text style={{ fontSize: 15, fontWeight: 'bold' }}>Other Reviews</Text>}
+						<ReviewList allReviews={otherReviews} />
+					</View>
+				</View>
+			</Card>
+
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    pressableButtonStyle: {
-        backgroundColor: 'tomato',
-        padding: 7,
-        borderRadius: 10,
-        marginTop: 5,
-        width: 200,
-        alignSelf: 'center'
+	pressableButtonStyle: {
+		backgroundColor: 'tomato',
+		padding: 7,
+		borderRadius: 10,
+		marginTop: 5,
+		width: 200,
+		alignSelf: 'center'
 
-    }
+	}
 })
