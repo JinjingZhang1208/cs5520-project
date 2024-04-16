4905c4ff98c82b97fdbd7c38e4f146dca97e8b10
JinjingZhang1208 <zhang.jinjin@northeastern.edu>
2024-04-16 14:20:42 -0700

    update the go back navigation in the notification screen

--- screens/Notification.js
+++ screens/Notification.js
@@ -1,11 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 import CommonStyles from '../styles/CommonStyles'
+import { useNavigation } from '@react-navigation/native'
 
 const Notification = () => {
+  const navigation = useNavigation()
+
   return (
-    <View style={[CommonStyles.container, {marginTop: 80}]}>
+    <View style={[CommonStyles.container, { marginTop: 80 }]}>
       <Text>We will push you notifications here!</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   )
 }
